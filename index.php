57825487b661e01a1f591e080cb6f17ddc3c4672
Arief Hilmi <arief.hilmi@gmail.com>
2018-12-18 11:25:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
 		</style>
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" 
 			  integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">		
-		<link href='https://fonts.googleapis.com/css?family=Cinzel' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Cinzel:700' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<div class="container">
