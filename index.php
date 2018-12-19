d8d3f469d8d8fdc0f321ee9a58bd9eb49930f48c
Arief Hilmi <arief.hilmi@gmail.com>
2018-12-19 10:40:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" 
 			  integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">		
 		<link href='https://fonts.googleapis.com/css?family=Cinzel:700' rel='stylesheet' type='text/css'>		
-		<link rel='shortcut icon' type='image/x-icon' href='favicon.ico' />
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 	</head>
 	<body>
 		<div class="container">
