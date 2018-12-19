2dba6e298995d681b74be7c1ca52bb02f864290e
Arief Hilmi <arief.hilmi@gmail.com>
2018-12-19 10:38:00 +0800

    update code

--- index.php
+++ index.php
@@ -64,7 +64,8 @@
 		</style>
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" 
 			  integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">		
-		<link href='https://fonts.googleapis.com/css?family=Cinzel:700' rel='stylesheet' type='text/css'>		
+		<link href='https://fonts.googleapis.com/css?family=Cinzel:700' rel='stylesheet' type='text/css'>
+		<link rel="shortcut icon" type="image/png" href="favicon.ico"/>
 	</head>
 	<body>
 		<div class="container">
