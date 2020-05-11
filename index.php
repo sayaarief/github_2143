d946c9fe26833f86a0537e59d2ddac8a7ebd5e94
sayaarief <arief.hilmi@gmail.com>
2020-05-11 21:45:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 	<head>
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
 				crossorigin="anonymous"></script>
-		<script src="TimezZ-master/src/timezz.min.js"></script>
+		<!--<script src="TimezZ-master/src/timezz.min.js"></script>-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.12.0/moment.js"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" 
 				integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
 		<script type="text/javascript">
@@ -19,7 +20,7 @@
 					stop: false,
 				});
 				
-				new TimezZ('.ariesa', {
+				new TimezZ('.ariesa_days', {
 					date: 'April 3, 2020 00:01:00',
 					daysName: 'd',
 					hoursName: 'h',
@@ -88,7 +89,9 @@
 						<h2>Nur Ariesa Auliya</h2>
 						<h2>3<sup>rd</sup> April 2020</h2>
 						<hr/>
-						<h2><div align="center" class="ariesa"></div></h2>
+						<h2><div align="center" class="ariesa_days"></div></h2>
+						<h2><div align="center" class="ariesa_month"></div></h2>
+						<h2><div align="center" class="ariesa_year"></div></h2>
 					</div>
 				</div>				
 			</div>		  
